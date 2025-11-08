e6c1211e025730555f9e57b2208360c8dc209000
Ericuwagboe15 <ericuwagboe4@gmail.com>
2025-11-08 13:20:40 +0100

    d

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,10 @@
-const currentYear = new Date().getFullYear();
-document.getElementById("currentYear").innerHTML = 'Last Modified, ';
-const lastModifieDate = document.lastModified;
-document.getElementById("lastModified").innerHTML = lastModifieDate;
-
-
 const lastModified = new Date(document.lastModified);
-const options = { year: 'numeric', month: 'long', day: 'numeric'};
-document.getElementById('lastModified').textContent = lastModified.toLocaleDateString('en-Us', options)
+const options = {year: 'numeric', month: 'long', day: 'numeric', time: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('lastModified').innerHTML = 'Last Modified: ' + lastModified.toLocaleString('en-Us', options)
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
