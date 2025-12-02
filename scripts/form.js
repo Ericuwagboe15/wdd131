cdf1698e301d745295e6f1d5dabbfa8289d0a9cb
Ericuwagboe15 <ericuwagboe4@gmail.com>
2025-12-02 11:20:27 +0100

    d

--- scripts/form.js
+++ scripts/form.js
@@ -35,7 +35,6 @@ products.forEach(product => {
     select.appendChild(option);
 });
 
-
 const lastModified = new Date(document.lastModified);
 const options = { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric"};
 document.getElementById("lastModified").innerHTML = "Last Modified " + lastModified.toLocaleString("en-us", options);
